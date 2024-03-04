da7fbcb7c20ad0a11b3c15e43accdeca87f1a857
Upendra5114 <145817678+Upendra5114@users.noreply.github.com>
2024-03-04 20:12:10 +0530

    diffinh.java

--- diffinh.java
+++ diffinh.java
@@ -1,40 +1,106 @@
-mport java.util.Scanner;
+import java.util.Scanner;
+// Simple Inheritance
+class Calculator {
+    protected double a, b;
 
-class Calculator{
-    void sub(double a, double b)
-    {
-        System.out.println("a-b: "+(a-b));
-    }
-    void add(double a, double b)
-    {
-        System.out.println("a+b: "+(a+b));
-    }
-    
-}
-class AdvCalculator extends Calculator{
-    void  mul(double a, double b)
-    {
-        System.out.println("a*b: "+(a*b));
-    }
-    void div(double a, double b)
-    {
-        System.out.println("a/b: "+(a/b));
+    public void add(double a, double b) {
+        System.out.println("a+b: " + (a + b));
     }
 
+    public void sub(double a, double b) {
+        System.out.println("a-b: " + (a - b));
+    }
 }
-// it is not possible for private Inheritance and protected inheritance....by default it is public 
-public class Program6 {
+
+class AdvCalculator extends Calculator {
+    public void mul(double a, double b) {
+        System.out.println("a*b: " + (a * b));
+    }
+
+    public void div(double a, double b) {
+        System.out.println("a/b: " + (a / b));
+    }
+}
+
+// Multiple Inheritance not possible in java
+// Hierarchical Inheritance
+class Rajamouli {
+    public void displayRajamouli() {
+        System.out.println("I'm Rajamouli");
+    }
+}
+
+class Chatrapathi extends Rajamouli {
+    public void displayChatrapathi() {
+        displayRajamouli();
+        System.out.println("I'm the director of Chatrapthi");
+    }
+}
+
+class Bahubali extends Rajamouli {
+    public void displayBahubali() {
+        displayRajamouli();
+        System.out.println("I'm the director of Bahubali");
+    }
+}
+
+class RRR extends Rajamouli {
+    public void displayRRR() {
+        displayRajamouli();
+        System.out.println("I'm the director of RRR");
+    }
+
+}
+
+// Multilevel Inheritance
+class Egg {
+    public void displayEgg() {
+        System.out.println("I'm an egg");
+    }
+}
+
+class Hen extends Egg {
+    public void displayHen() {
+        System.out.println("I'm the hen who is the next version of the egg");
+    }
+}
+
+class Chicken extends Hen {
+    public void displayChicken() {
+        displayEgg();
+        displayHen();
+        System.out.println("I'm the chicken... my journey started with egg --> hen --> chicken");
+    }
+}
+
+//Hybrid inheritance is not possible in java
+
+public class Program7 {
     public static void main(String[] args) {
-        System.out.println();
-        AdvCalculator obj = new AdvCalculator();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter the operands");
-        double a= input.nextDouble();
-        double b= input.nextDouble();
-        input.close();
-        obj.add(a, b);
-        obj.sub(a, b);
-        obj.mul(a, b);
-        obj.div(a, b);
+        AdvCalculator obj1 = new AdvCalculator();
+        System.out.println("--------------------------------------------------------------------------------->");
+        System.out.println("Simple Inheritance: ");
+        System.out.println("Enter the operands: ");
+        Scanner scanner = new Scanner(System.in);
+        obj1.a = scanner.nextDouble();
+        obj1.b = scanner.nextDouble();
+        obj1.add(obj1.a, obj1.b);
+        obj1.sub(obj1.a, obj1.b);
+        obj1.mul(obj1.a, obj1.b);
+        obj1.div(obj1.a, obj1.b);
+        System.out.println("----------------------------------------------------------------------------------------------------->");
+        System.out.println("Hierarchical Inheritance: ");
+        Chatrapathi obj3 = new Chatrapathi();
+        obj3.displayChatrapathi();
+        Bahubali obj4 = new Bahubali();
+        obj4.displayBahubali();
+        RRR obj5 = new RRR();
+        obj5.displayRRR();
+        System.out.println("----------------------------------------------------------------------------------------------------->");
+        System.out.println("MultiLevel Inheritance: ");
+        Chicken obj6 = new Chicken();
+        obj6.displayChicken();
+        System.out.println("----------------------------------------------------------------------------------------------------->");
+        scanner.close();
     }
 }
